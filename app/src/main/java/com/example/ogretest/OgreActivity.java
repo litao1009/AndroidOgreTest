08a75b411a725fd3fd531ad2db93ddb3d8622cc4
lee <lee@email.com>
2016-11-23 18:22:56 +0800

    -init

---
+++ app/src/main/java/com/example/ogretest/OgreActivity.java
@@ -0,0 +1,16 @@
+package com.example.ogretest;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+
+public class OgreActivity extends NativeActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
