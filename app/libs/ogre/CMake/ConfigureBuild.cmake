92cde3f3b1057043a0eca8163df85ec3b1a1b233
lee <lee@email.com>
2016-12-02 15:49:50 +0800

    -update

--- app/libs/ogre/CMake/ConfigureBuild.cmake
+++ app/libs/ogre/CMake/ConfigureBuild.cmake
@@ -16,13 +16,6 @@
 set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DDEBUG")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG")
 
-if (OGRE_BUILD_PLATFORM_APPLE_IOS)
-  set(OGRE_SET_BUILD_PLATFORM_APPLE_IOS 1)
-  set(OGRE_STATIC TRUE)
-  set(OGRE_STATIC_LIB TRUE)
-  set(OGRE_CONFIG_ENABLE_PVRTC TRUE)
-endif()
-
 # should we build static libs?
 if (OGRE_STATIC)
   set(OGRE_LIB_TYPE STATIC)
@@ -32,41 +25,6 @@ endif ()
 
 # configure threading options
 set(OGRE_THREAD_PROVIDER 0)
-if (OGRE_CONFIG_THREADS)
-	if (UNIX)
-		add_definitions(-pthread)
-	endif ()
-
-	if (OGRE_CONFIG_THREAD_PROVIDER STREQUAL "boost")
-		set(OGRE_THREAD_PROVIDER 1)
-		include_directories(${Boost_INCLUDE_DIRS})
-		# On MSVC Boost usually tries to autolink boost libraries. However since
-		# this behaviour is not available on all compilers, we need to find the libraries
-		# ourselves, anyway. Disable auto-linking to avoid mess-ups.
-		add_definitions(-DBOOST_ALL_NO_LIB)
-        if (MINGW AND Boost_USE_STATIC_LIBS)
-            # mingw needs this to link against static thread libraries
-            add_definitions(-DBOOST_THREAD_USE_LIB)
-        endif ()
-		set(OGRE_THREAD_LIBRARIES ${Boost_LIBRARIES})
-	endif ()
-
-	if (OGRE_CONFIG_THREAD_PROVIDER STREQUAL "poco")
-		set(OGRE_THREAD_PROVIDER 2)
-		include_directories(${POCO_INCLUDE_DIRS})
-		set(OGRE_THREAD_LIBRARIES ${POCO_LIBRARIES})
-	endif ()
-
-	if (OGRE_CONFIG_THREAD_PROVIDER STREQUAL "tbb")
-		set(OGRE_THREAD_PROVIDER 3)
-		include_directories(${TBB_INCLUDE_DIRS})
-		if (WIN32 AND MINGW)
-			add_definitions(-D_WIN32_WINNT=0x0501)    
-		endif ()
-
-		set(OGRE_THREAD_LIBRARIES ${TBB_LIBRARIES})
-	endif ()
-endif()
 
 set(OGRE_ASSERT_MODE 0 CACHE STRING 
 	"Enable Ogre asserts and exceptions. Possible values:
@@ -206,113 +164,3 @@ endif()
 # generate OgreBuildSettings.h
 configure_file(${OGRE_TEMPLATES_DIR}/OgreBuildSettings.h.in ${OGRE_BINARY_DIR}/include/OgreBuildSettings.h @ONLY)
 install(FILES ${OGRE_BINARY_DIR}/include/OgreBuildSettings.h DESTINATION include/OGRE)
-
-
-# Create the pkg-config package files on Unix systems
-if (UNIX)
-  set(OGRE_LIB_SUFFIX "")
-  set(OGRE_PLUGIN_PREFIX "")
-  set(OGRE_PLUGIN_EXT ".so")
-  set(OGRE_PAGING_ADDITIONAL_PACKAGES "")
-  if (OGRE_STATIC)
-    set(OGRE_LIB_SUFFIX "${OGRE_LIB_SUFFIX}Static")
-    set(OGRE_PLUGIN_PREFIX "lib")
-    set(OGRE_PLUGIN_EXT ".a")
-  endif ()
-  if (OGRE_BUILD_TYPE STREQUAL "debug")
-    set(OGRE_LIB_SUFFIX "${OGRE_LIB_SUFFIX}_d")
-  endif ()
-
-  set(OGRE_ADDITIONAL_LIBS "")
-  set(OGRE_CFLAGS "")
-  set(OGRE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
-  if (OGRE_CONFIG_THREADS GREATER 0)
-    set(OGRE_CFLAGS "-pthread")
-    set(OGRE_ADDITIONAL_LIBS "${OGRE_ADDITIONAL_LIBS} -lpthread")
-  endif ()
-  if (OGRE_STATIC)
-    if (OGRE_CONFIG_THREADS)
-      set(OGRE_ADDITIONAL_LIBS "${OGRE_ADDITIONAL_LIBS} -lboost-thread-mt")
-    endif ()
-    # there is no pkgconfig file for freeimage, so we need to add that lib manually
-    set(OGRE_ADDITIONAL_LIBS "${OGRE_ADDITIONAL_LIBS} -lfreeimage")
-    configure_file(${OGRE_TEMPLATES_DIR}/OGREStatic.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE.pc @ONLY)
-  else ()
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE.pc @ONLY)
-  endif ()
-  install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-
-  # configure additional packages
-
-  if (OGRE_BUILD_COMPONENT_PAGING)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-Paging.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Paging.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Paging.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (OGRE_BUILD_COMPONENT_MESHLODGENERATOR)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-MeshLodGenerator.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-MeshLodGenerator.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-MeshLodGenerator.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-  
-  if (OGRE_BUILD_COMPONENT_TERRAIN)
-    if (OGRE_BUILD_COMPONENT_PAGING)
-      set(OGRE_PAGING_ADDITIONAL_PACKAGES ", OGRE-Paging = ${OGRE_VERSION}")
-    endif ()
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-Terrain.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Terrain.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Terrain.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (OGRE_BUILD_COMPONENT_RTSHADERSYSTEM)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-RTShaderSystem.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-RTShaderSystem.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-RTShaderSystem.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (OGRE_BUILD_COMPONENT_PROPERTY)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-Property.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Property.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Property.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (OGRE_BUILD_COMPONENT_OVERLAY)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-Overlay.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Overlay.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Overlay.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (OGRE_BUILD_COMPONENT_VOLUME)
-    configure_file(${OGRE_TEMPLATES_DIR}/OGRE-Volume.pc.in ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Volume.pc @ONLY)
-    install(FILES ${OGRE_BINARY_DIR}/pkgconfig/OGRE-Volume.pc DESTINATION ${OGRE_LIB_DIRECTORY}/pkgconfig)
-  endif ()
-
-  if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(CMAKE_COMPILER_IS_CLANGXX 1)
-  endif ()
-
-endif ()
-
-if(OGRE_CONFIG_STATIC_LINK_CRT)
-#We statically link to reduce dependencies
-foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-    if(${flag_var} MATCHES "/MD")
-        string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-    endif(${flag_var} MATCHES "/MD")
-    if(${flag_var} MATCHES "/MDd")
-        string(REGEX REPLACE "/MDd" "/MTd" ${flag_var} "${${flag_var}}")
-    endif(${flag_var} MATCHES "/MDd")
-endforeach(flag_var)
-endif(OGRE_CONFIG_STATIC_LINK_CRT)
-
-### Commented because the FindOGRE script can currently fill this role better ###
-# # Create the CMake package files
-# if (WIN32)
-#   set(OGRE_CMAKE_DIR CMake)
-# elseif (UNIX)
-#   set(OGRE_CMAKE_DIR lib/cmake)
-# elseif (APPLE)
-# endif ()
-# configure_file(${OGRE_TEMPLATES_DIR}/OGREConfig.cmake.in ${OGRE_BINARY_DIR}/cmake/OGREConfig.cmake @ONLY)
-# configure_file(${OGRE_TEMPLATES_DIR}/OGREConfigVersion.cmake.in ${OGRE_BINARY_DIR}/cmake/OGREConfigVersion.cmake @ONLY)
-# install(FILES
-#   ${OGRE_BINARY_DIR}/cmake/OGREConfig.cmake
-#   ${OGRE_BINARY_DIR}/cmake/OGREConfigVersion.cmake
-#   DESTINATION ${OGRE_CMAKE_DIR}
-# )
-#
--- app/libs/ogre/CMake/Utils/OgreAddTargets.cmake
+++ app/libs/ogre/CMake/Utils/OgreAddTargets.cmake
@@ -53,43 +53,6 @@ macro(create_unity_build_files TARGETNAME)
   endforeach()
   set(_SOURCES ${_PRIMARY} ${_EXCLUDES})
   list(REMOVE_DUPLICATES _SOURCES)
-
-  if (OGRE_UNITY_BUILD)
-    include_directories(${CMAKE_CURRENT_SOURCE_DIR})
-    # create Unity compilation units
-    # all source files given will be put into a certain number of
-    # compilation units.
-    # if certain source files should be excluded from the unity build
-    # and built separately, they need to also be named in the SEPARATE
-    # list.
-    set(_FILE_NUM 0)
-    set(_FILE_CNT 0)
-    set(_FILE_CONTENTS "")
-    message(STATUS "Creating Unity build files for target ${TARGETNAME}")
-    foreach(_FILE ${_PRIMARY})
-      # test if file is more than just a header
-      get_filename_component(_EXT ${_FILE} EXT)
-      list(FIND _EXCLUDES ${_FILE} _EXCLUDED)
-      if ((_EXT STREQUAL ".cpp") AND (_EXCLUDED EQUAL "-1"))
-        set(_FILE_CONTENTS "${_FILE_CONTENTS}\#include \"${_FILE}\"\n")
-        math(EXPR _FILE_CNT "${_FILE_CNT}+1")
-        if(_FILE_CNT EQUAL OGRE_UNITY_FILES_PER_UNIT)
-          set(_FILENAME "${OGRE_BINARY_DIR}/${TARGETNAME}/compile_${TARGETNAME}_${_FILE_NUM}.cpp")
-          check_and_update_file(${_FILENAME} ${_FILE_CONTENTS})
-          math(EXPR _FILE_NUM "${_FILE_NUM}+1")
-          set(_FILE_CNT 0)
-          set (_FILE_CONTENTS "")
-          list(APPEND _SOURCES ${_FILENAME})
-        endif()
-        # exclude the original source file from the compilation
-        set_source_files_properties(${_FILE} PROPERTIES LANGUAGE "" HEADER_FILE_ONLY TRUE)
-      endif()
-    endforeach()
-    # don't forget the last set of files
-    set(_FILENAME "${OGRE_BINARY_DIR}/${TARGETNAME}/compile_${TARGETNAME}_${_FILE_NUM}.cpp")
-    check_and_update_file(${_FILENAME} ${_FILE_CONTENTS})
-    list(APPEND _SOURCES ${_FILENAME})
-  endif ()
 endmacro()
 
 
@@ -99,24 +62,3 @@ function(ogre_add_library TARGETNAME LIBTYPE)
   create_unity_build_files(${TARGETNAME} ${ARGN})
   add_library(${TARGETNAME} ${LIBTYPE} ${_SOURCES})
 endfunction(ogre_add_library)
-
-
-# add a new executable target
-# usage: ogre_add_executable(TARGETNAME [WIN32] [MACOSX_BUNDLE] SOURCE_FILES [SEPARATE SOURCE_FILES])
-function(ogre_add_executable TARGETNAME)
-  # test if WIN32 or MACOSX_BUNDLE options were provided
-  set(_WIN32 "")
-  set(_OSX "")
-  list(FIND ARGN "WIN32" _W32_IDX)
-  if (_W32_IDX GREATER "-1")
-    set(_WIN32 "WIN32")
-    list(REMOVE_AT ARGN ${_W32_IDX})
-  endif ()
-  list(FIND ARGN "MACOSX_BUNDLE" _OSX_IDX)
-  if (_OSX_IDX GREATER "-1")
-    set(_OSX "MACOSX_BUNDLE")
-    list(REMOVE_AT ARGN ${_OSX_IDX})
-  endif ()
-  create_unity_build_files(${TARGETNAME} ${ARGN})
-  add_executable(${TARGETNAME} ${_WIN32} ${_OSX} ${_SOURCES})
-endfunction()
